a588cf5953ff2fc40b106d57f38c50aac2da7f5b
lucasreisbr <lucas@lucasreis.com.br>
2024-09-12 13:34:45 -0300

    TitleEffect

--- src/Pages/Polos.js
+++ src/Pages/Polos.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from '../Modules/Footer';
 import Accordionpolo from '../Modules/Accordionpolo';
 
 function Polos() {
+
+    useEffect(() => {
+      document.title = "Locais de Prova em São Paulo, Minas Gerais, Paraná e Pasrá | IBRESP 2024";
+      
+      // Set meta description
+      const metaDescription = document.querySelector('meta[name="description"]');
+      if (metaDescription) {
+        metaDescription.setAttribute("content", "Confira os locais de provas do Ibresp. São mais de 30 locais de provas espalhados pelo Estado de São Paulo, Minas, Paraná e Pará. Confira aqui os endereços.");
+      } else {
+        const newMetaDescription = document.createElement('meta');
+        newMetaDescription.name = "description";
+        newMetaDescription.content = "Confira os locais de provas do Ibresp. São mais de 30 locais de provas espalhados pelo Estado de São Paulo, Minas, Paraná e Pará. Confira aqui os endereços.";
+        document.head.appendChild(newMetaDescription);
+      }
+    }, []);
+
     return(
 <div>
   <div id="top" className=" flex flex-row justify-center p-3 lg:p-0">
